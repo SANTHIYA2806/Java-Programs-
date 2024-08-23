f0ba7bf1b45736112c067c14c88a4204da700df1
SANTHIYA2806 <138847027+SANTHIYA2806@users.noreply.github.com>
2024-08-23 15:00:45 +0530

    Update armstrong.java

--- armstrong.java
+++ armstrong.java
@@ -4,19 +4,25 @@ public class armstrong{
         System.out.println("Enter the number: ");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        int temp, rem;
-        int sum = 0;
-        temp = num;
-        while(num!= 0){
-            rem = num%10;
-            sum = sum + rem*rem*rem;
-            num = num/10;
+        int temp1, rem,temp2;
+        int count = 0;
+        double sum = 0;
+        temp1 = num;
+        temp2 = num;
+        while (num >0) {
+            num = num / 10;
+            count++;
         }
-        if(temp==sum){
-            System.out.println(temp+" is an armstrong number");
+        while(temp1!= 0){
+            rem = temp1%10;
+            sum = (sum + Math.pow(rem,count));
+            temp1 = temp1/10;
+        }
+        if(temp2==sum){
+            System.out.println(temp2+" is an armstrong number");
         }
         else{
-            System.out.println(temp+" is not an armstrong number");
+            System.out.println(temp2+" is not an armstrong number");
         }
     }
 }
