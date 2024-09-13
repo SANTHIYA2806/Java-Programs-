a720abd17e0b81ec48392bb2c057947a26019cd1
SANTHIYA2806 <138847027+SANTHIYA2806@users.noreply.github.com>
2024-09-13 12:12:49 +0530

    Create createnode.java

---
+++ Linked List/createnode.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+}
+public class ds {
+    Node head;
+
+    void Create(int n) {
+        Node temp, newnode;
+        head = new Node();
+        Scanner sc = new Scanner(System.in);
+        int data = sc.nextInt();
+        head.data = data;
+        head.next = null;
+        temp = head;
+        for (int i = 1; i < n; i++) {
+            newnode = new Node();
+        }
+    }
+
+    void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ds s = new ds();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        s.Create(n);
+        s.display();
+    }
+}
+
